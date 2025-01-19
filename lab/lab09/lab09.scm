81ef310c5ae1676f9c84d30de23e9effd39bd3b3
shadoxx152 <shadoxx152@gmail.com>
2025-01-19 17:21:34 +0800

    Complete lab09

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,17 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (cond ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    (else 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (if (= n 0)
+    (lambda (x) x)
+    (lambda (x) ((repeat f (- n 1))(f x)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +23,6 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (if (= 0 (min a b))
+    (max a b)
+    (gcd (min a b) (modulo (max a b) (min a b)))))
