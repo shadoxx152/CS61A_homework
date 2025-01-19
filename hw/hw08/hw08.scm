b8f7676f02b120a3db7c964c3d6fb07c30685141
shadoxx152 <shadoxx152@gmail.com>
2025-01-19 22:17:19 +0800

    Complete hw08

---
+++ hw/hw08/hw08.scm
@@ -0,0 +1,20 @@
+(define (ascending? s) 
+    (or (null? s)
+        (null? (cdr s))
+        (and (<= (car s) (car (cdr s))) (ascending? (cdr s)))))
+
+(define (my-filter pred s) 
+    (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))   
+
+(define (interleave lst1 lst2) 
+    (cond ((and (null? lst1) (null? lst2)) ())
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
+
+(define (no-repeats s) 
+    (if (null? s)
+        '()
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not(= x (car s)))) (cdr s))))))
