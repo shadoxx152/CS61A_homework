b950f42c7791bb541d1a530e9a60ef837130847a
shadoxx152 <shadoxx152@gmail.com>
2025-01-21 00:33:46 +0800

    Complete lab11

--- lab/lab11/lab11.scm
+++ lab/lab11/lab11.scm
@@ -1,15 +1,37 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  `(if ,condition
+      ,if-true
+      ,if-false))
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+;scm> (pow-expr 2 0)
+;1
+;scm> (pow-expr 2 1)
+;(* 2 1)
+;scm> (pow-expr 2 5)
+;(* 2 (square (square (* 2 1))))
+;scm> (pow-expr 2 15)
+;(* 2 (square (* 2 (square (* 2 (square (* 2 1)))))))
+;scm> (pow-expr 2 16)
+;(square (square (square (square (* 2 1)))))
+;scm> (eval (pow-expr 2 16))
+;65536
+(define (pow-expr base exp) 
+  (if (= exp 0)
+      1
+      (if (= exp 1)
+          `(* ,base 1)
+          (if (even? exp)
+              `(square ,(pow-expr base (/ exp 2)))
+              `(* ,base ,(pow-expr base (- exp 1)))))))
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr)))
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin (f)
+             (repeated-call (- n 1) f))))
